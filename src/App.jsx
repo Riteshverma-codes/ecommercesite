fcb0d5bd8cff512fa765d098f18b3d95fdde9581
riteshvermacodes <riteshverma.codes@gmail.com>
2025-04-10 16:09:58 +0530

    Add error component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import Home from './Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Error from './Components/Error'
 
 export default function App() {
   return (
@@ -10,7 +14,9 @@ export default function App() {
     <Navbar/>
     <Routes>
         <Route path ="" element={<Home/>}/>
-        <Route path ="" element={<Home/>}/>
+        <Route path='/about' element= {<About/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+        <Route path ="*" element={<Error/>} />
     </Routes>
     <Footer/>
     </BrowserRouter>
